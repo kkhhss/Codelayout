119fc23ef17e47044314a5de19048295c3fc5800
kkhhss <yg91238405@gmail.com>
2023-03-23 19:50:52 +0900

    Button Event Event Programming

--- app/src/main/java/kr/hs/emirim/kkhhss/codelayout/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/codelayout/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.hs.emirim.kkhhss.codelayout
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener{
+            Toast.makeText(applicationContext, "버튼을 눌렀어요", Toast.LENGTH_SHORT).show()
+        }
     }
 }

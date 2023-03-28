b95c6a20b7263584985eb7af0a01f41c51a3aaf6
kkhhss <yg91238405@gmail.com>
2023-03-28 19:48:17 +0900

    직접풀어보기 2-3 완성

--- app/src/main/java/kr/hs/emirim/kkhhss/codelayout/SecondActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/codelayout/SecondActivity.kt
@@ -1,11 +1,41 @@
 package kr.hs.emirim.kkhhss.codelayout
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btn4 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btn1 = findViewById<Button>(R.id.btn1)
+        btn2 = findViewById<Button>(R.id.btn2)
+        btn3 = findViewById<Button>(R.id.btn3)
+        btn4 = findViewById<Button>(R.id.btn4)
+
+        btn1.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"))
+            startActivity(intent)
+        }
+        btn2.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/010-9123-8405"))
+            startActivity(intent)
+        }
+        btn3.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
+            startActivity(intent)
+        }
+        btn4.setOnClickListener{
+            finish()
+        }
+
     }
 }
